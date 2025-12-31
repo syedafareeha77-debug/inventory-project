31870af17fbc3ba3ccfbf52c49c0664e49bd004f
syedafareeha77-debug <syedafareeha77@gmail.com>
2025-12-31 22:17:09 +0500

    a

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { getProducts, addProduct, updateProduct, deleteProduct } = require("../controllers/productController");
+const {
+  getProducts,
+  addProduct,
+  updateProduct,
+  deleteProduct,
+  restockAllProducts, // naya controller function
+} = require("../controllers/productController");
 const auth = require("../middleware/auth");
 
 // Protected routes
@@ -8,5 +14,8 @@ router.get("/", auth, getProducts);
 router.post("/", auth, addProduct);
 router.put("/:id", auth, updateProduct);
 router.delete("/:id", auth, deleteProduct);
+
+// --- New Route for Restock All ---
+router.put("/restock-all", auth, restockAllProducts);
 
 module.exports = router;
